3ed78b67a9400290ba62ee88ffcd2e90a50231fe
gauravverma9795 <gauravverma9795@gmail.com>
2024-12-24 01:09:58 +0530

    update add interview

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -30,6 +30,7 @@ function Header() {
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
+      <div className='flex items-center gap-6'>
       <ul className='hidden md:flex gap-6  '>
 
         <li onClick={MoveInDeshboard} className={`hover:text-primary hover:font-bold transition-all cursor-pointer
@@ -51,12 +52,10 @@ ${path == '/dashboard/upgrade' && 'text-primary font-bold'}
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer${path == '/dashboard/how' && 'text-primary font-bold'}`}>
           How it Works?
         </li>
-        <li>
-          <UserButton />
-        </li>
-
-
       </ul>
+          <UserButton />
+        </div>
+
 
     </div>
   )
